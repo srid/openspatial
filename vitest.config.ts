6a913023158744a600deaec110e5c2c289c1be6f
Sridhar Ratnakumar <srid@srid.ca>
2026-02-02 20:18:10 -0500

    d

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,6 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['**/*.test.ts'],
+    exclude: ['**/node_modules/**', '.direnv/**', 'result/**'],
   },
 });
